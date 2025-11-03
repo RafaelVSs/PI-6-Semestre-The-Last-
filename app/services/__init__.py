ff0e538e0fb4b62027dee0a0faf6b020a6f225b9
Rafael Veríssimo da Silva (Dev. SRE Seller) <rafael.verissimo@luizalabs.com>
2025-11-03 12:05:23 -0300

    feat: add services

---
+++ app/services/__init__.py
@@ -0,0 +1,6 @@
+from .user_service import UserService
+from .telephone_number_service import TelephoneNumberService
+from .refuel_service import RefuelService
+from .vehicle_service import VehicleService
+
+__all__ = ["UserService", "TelephoneNumberService", "RefuelService", "VehicleService"]
